cdb96637da3ca794fdd63e9f975f4c32c0ab73ce
Dmitriy Kopylenko <dima767@gmail.com>
2020-12-29 14:32:37 -0500

    Modules; Modifiy UI behavior

---
+++ go.mod
@@ -0,0 +1,5 @@
+module y2kpos
+
+go 1.15
+
+require github.com/rivo/tview v0.0.0-20201204190810-5406288b8e4e
--- y2kpos.go
+++ y2kpos.go
@@ -1,34 +1,50 @@
 package main
+
 //import "github.com/gdamore/tcell"
 import (
+	"os"
+
 	"github.com/rivo/tview"
+)
+
+var (
+	form  *tview.Form
+	modal *tview.Modal
+)
+
+const (
+	usernameIndex = iota
+	passwordIndex
+	loginButtonIndex
+	exitButtonIndex
 )
 
 func main() {
 	app := tview.NewApplication()
 
-	modal := tview.NewModal().
+	modal = tview.NewModal().
 		SetText("Do you want to quit the application?").
 		AddButtons([]string{"Quit", "Cancel"}).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-			if buttonLabel == "Quit" {
+			if buttonIndex == 0 {
 				app.Stop()
+				os.Exit(0)
+			} else {
+				app.SetRoot(form, true).SetFocus(form.SetFocus(usernameIndex))
 			}
+
 		})
 
-	form := tview.NewForm().
+	form = tview.NewForm().
 		AddInputField("Username", "", 20, nil, nil).
 		AddPasswordField("Password", "", 20, '*', nil).
 		AddButton("Login", func() {
-			app.SetRoot(modal, false).SetFocus(modal).Run();
+			app.SetRoot(modal, false).SetFocus(modal).Run()
 		}).
 		AddButton("Exit", func() {
 			app.Stop()
 		})
 	form.SetBorder(true).SetTitle("Login").SetTitleAlign(tview.AlignLeft)
-
-
-
 
 	if err := app.SetRoot(form, true).SetFocus(form).Run(); err != nil {
 		panic(err)
